daaea8bcd3575367b0368d52545c9b356f8a8bde
Hacker T <ttt9675@gmail.com>
2022-01-14 15:00:34 +0400

    6_3.cpp build commit

---
+++ I/6_3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout<<"Enter two number:"<<endl;
+	float Num1=0, Num2=0;
+	cin>>Num1;
+	cin>>Num2;
+	
+	cout<<"Enter 'd' to divide, anything else to multyply: ";
+	char UserSelection='\0';
+	cin>>UserSelection;
+	
+	if(UserSelection=='d')
+	{
+		cout<<"You want division!"<<endl;
+		if(Num2!=0)
+		{
+		cout<<"no div-by-zero, proceeding to calculate"<<endl;
+		cout<< Num1<<"/"<<Num2<<"="<<Num1/Num2<<endl;
+		}
+		else
+		cout<<"Division by zero is not allowed"<<endl;
+		}
+		else
+		{
+			cout<<"You want multiplication!"<<endl;
+		cout<< Num1<<"x"<<Num2<<"="<<Num1*Num2<<endl;
+		}
+	return 0;
+	}
